f90305fa680c829e7eea788f38c86d4699e44e2c
thonbecker <me@thonbecker.com>
2025-02-07 19:26:40 -0600

    Initialize Spring Boot project with basic setup
    Add Spring Boot application, configuration files, and Maven setup. Include dependencies for Thymeleaf, web, and testing. Configure tools like SortPom and Spotless to ensure code consistency.

---
+++ src/test/java/solutions/thonbecker/personal/PersonalApplicationTests.java
@@ -0,0 +1,11 @@
+package solutions.thonbecker.personal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
