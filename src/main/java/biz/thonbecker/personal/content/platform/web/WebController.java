2616e76733da5c515c61a7bceb7915e90822d9d3
Thon Becker <thon.becker@gmail.com>
2026-07-10 23:14:25 -0500

    Remove obsolete Spring homepage

--- src/main/java/biz/thonbecker/personal/content/platform/web/WebController.java
+++ src/main/java/biz/thonbecker/personal/content/platform/web/WebController.java
@@ -6,11 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class WebController {
 
-    @GetMapping("/")
-    public String index() {
-        return "redirect:https://thonbecker.biz";
-    }
-
     @GetMapping("/trivia")
     public String trivia() {
         return "trivia";
