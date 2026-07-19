4c39fca7bdcff634f6d303794c1d2a636145f0ba
Thon Becker <thon.becker@gmail.com>
2026-07-18 20:13:21 -0500

    Render magic link login page

--- src/main/java/biz/thonbecker/personal/user/platform/web/AuthenticationController.java
+++ src/main/java/biz/thonbecker/personal/user/platform/web/AuthenticationController.java
@@ -8,10 +8,15 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.time.Duration;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@RestController
+@Controller
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 class AuthenticationController {
