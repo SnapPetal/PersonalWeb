6b4fe5e3ac597ada304c29f31f7c2bac82101a8a
Thon Becker <thon.becker@gmail.com>
2026-08-16 06:31:55 -0500

    Fix PostHog properties bean registration

--- src/main/java/biz/thonbecker/personal/analytics/platform/configuration/PostHogConfig.java
+++ src/main/java/biz/thonbecker/personal/analytics/platform/configuration/PostHogConfig.java
@@ -1,8 +1,6 @@
 package biz.thonbecker.personal.analytics.platform.configuration;
 
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableConfigurationProperties(PostHogProperties.class)
 class PostHogConfig {}
